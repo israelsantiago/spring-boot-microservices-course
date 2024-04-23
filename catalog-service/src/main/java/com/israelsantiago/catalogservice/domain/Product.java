577daf83bda47c5455e3025bd2724d22ed38a64b
Israel Santiago <israel.santiago75@gmail.com>
2024-04-22 22:53:35 -0300

    Implement Get Products API with pagination.

---
+++ catalog-service/src/main/java/com/israelsantiago/catalogservice/domain/Product.java
@@ -0,0 +1,6 @@
+package com.israelsantiago.catalogservice.domain;
+
+import java.math.BigDecimal;
+
+public record Product(String code, String name, String description, String imageUrl, BigDecimal price) {
+}
