34e188cf69ff653cc6ca3af2cda625bd74343acf
Israel Santiago <israel.santiago75@gmail.com>
2024-04-22 22:53:35 -0300

    Implement Get Products API with pagination.

---
+++ catalog-service/src/main/java/com/israelsantiago/catalogservice/web/controllers/ProductController.java
@@ -0,0 +1,32 @@
+package com.israelsantiago.catalogservice.web.controllers;
+
+import com.israelsantiago.catalogservice.domain.PagedResult;
+import com.israelsantiago.catalogservice.domain.Product;
+import com.israelsantiago.catalogservice.domain.ProductNotFoundException;
+import com.israelsantiago.catalogservice.domain.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/products")
+class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    PagedResult<Product> getProducts(@RequestParam(name = "page", defaultValue = "1") int pageNo) {
+        return productService.getProducts(pageNo);
+    }
+
+    @GetMapping("/{code}")
+    ResponseEntity<Product> getProductByCode(@PathVariable String code) {
+        return productService
+                .getProductByCode(code)
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> ProductNotFoundException.forCode(code));
+    }
+}
