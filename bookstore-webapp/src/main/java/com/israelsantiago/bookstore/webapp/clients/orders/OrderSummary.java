2f35869c94bb21b287b86c260ce1f8e15e90e51f
Israel Santiago <israel.santiago75@gmail.com>
2024-04-27 15:04:25 -0300

    feat Added webapp service

---
+++ bookstore-webapp/src/main/java/com/israelsantiago/bookstore/webapp/clients/orders/OrderSummary.java
@@ -0,0 +1,3 @@
+package com.israelsantiago.bookstore.webapp.clients.orders;
+
+public record OrderSummary(String orderNumber, OrderStatus status) {}
---
+++ bookstore-webapp/src/main/java/com/israelsantiago/bookstore/webapp/web/controllers/OrderController.java
@@ -0,0 +1,59 @@
+package com.israelsantiago.bookstore.webapp.web.controllers;
+
+import com.israelsantiago.bookstore.webapp.clients.orders.CreateOrderRequest;
+import com.israelsantiago.bookstore.webapp.clients.orders.OrderConfirmationDTO;
+import com.israelsantiago.bookstore.webapp.clients.orders.OrderDTO;
+import com.israelsantiago.bookstore.webapp.clients.orders.OrderServiceClient;
+import com.israelsantiago.bookstore.webapp.clients.orders.OrderSummary;
+import jakarta.validation.Valid;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class OrderController {
+    private final OrderServiceClient orderServiceClient;
+
+    OrderController(OrderServiceClient orderServiceClient) {
+        this.orderServiceClient = orderServiceClient;
+    }
+
+    @GetMapping("/cart")
+    String cart() {
+        return "cart";
+    }
+
+    @PostMapping("/api/orders")
+    @ResponseBody
+    OrderConfirmationDTO createOrder(@Valid @RequestBody CreateOrderRequest orderRequest) {
+        return orderServiceClient.createOrder(orderRequest);
+    }
+
+    @GetMapping("/orders/{orderNumber}")
+    String showOrderDetails(@PathVariable String orderNumber, Model model) {
+        model.addAttribute("orderNumber", orderNumber);
+        return "order_details";
+    }
+
+    @GetMapping("/api/orders/{orderNumber}")
+    @ResponseBody
+    OrderDTO getOrder(@PathVariable String orderNumber) {
+        return orderServiceClient.getOrder(orderNumber);
+    }
+
+    @GetMapping("/orders")
+    String showOrders() {
+        return "orders";
+    }
+
+    @GetMapping("/api/orders")
+    @ResponseBody
+    List<OrderSummary> getOrders() {
+        return orderServiceClient.getOrders();
+    }
+}
