3f3d761bd67eb216b3049665d83d5cf00ab8fade
Israel Santiago <israel.santiago75@gmail.com>
2024-05-01 19:48:18 -0300

    Adjust formating

--- bookstore-webapp/src/main/java/com/israelsantiago/bookstore/webapp/config/SecurityConfig.java
+++ bookstore-webapp/src/main/java/com/israelsantiago/bookstore/webapp/config/SecurityConfig.java
@@ -5,8 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;  // bad practice, don't do this in production
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;  // bad practice, don't do this in production
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
